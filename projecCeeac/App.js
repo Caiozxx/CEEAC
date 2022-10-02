4afec240c5486c45ded2dee53cf2cffa1e6a7195
Caio Ferreira <fcaio127@uni9.edu.br>
2022-10-02 20:55:45 -0300

    Update App.js

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { CreateNativeStackNavigator } from '@react-navigation/native-stack';
 
-import templates from './templates/form';
+import Form from './templates/form';
+import Home from './templates/home';
 
 const Stack = CreateNativeStackNavigator();
 
 export default function app(){
   return(
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='home'>
-        <Stack.Screen name="home" component={home}/>
-        <Stack.Screen name="form" component={form}/>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Form" component={Form}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
