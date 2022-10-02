dc9bde82bfb8ebc3a174df0e932b3fd055a9b789
Caio Ferreira <fcaio127@uni9.edu.br>
2022-10-02 20:45:09 -0300

    updt

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { CreateNativeStackNavigator } from '@react-navigation/native-stack';
 
-import templates from './templates/home';
 import templates from './templates/form';
 
 const Stack = CreateNativeStackNavigator();
