049f9cf8aa48670dfe09e0e03ba2fadc0e053a08
Caio Ferreira <fcaio127@uni9.edu.br>
2022-11-21 18:17:46 -0300

    up carousel

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react'
+import {View, StyleSheet, Dimensions } from 'react-native'
+import Carousel from 'react-native-snap-carousel'
 
-import Home from './templates/Home';
+const SLIDER_WIDTH = Dimensions.get('window').width
+const ITEM_WIDTH = SLIDER_WIDTH * 0.88
 
-const Stack = createNativeStackNavigator();
+const carouselItems = [
+    {
+        imgUrl:
+        'https://pixabay.com/pt/photos/jaguar-gato-selvagem-felino-retrato-2894706/'
+    },
+    {
+        imgUrl:
+        'https://pixabay.com/pt/photos/praia-mar-p%c3%b4r-do-sol-crep%c3%basculo-99388/'
+    },
+    {
+        imgUrl:
+        'https://pixabay.com/pt/photos/p%c3%a3o-de-a%c3%a7%c3%bacar-bonde-bondinho-1679285/'
+    }
+]
 
-export default function app(){
-  return(
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export function Home(){
+    return (
+        <View style={styles.container}>
+             <Carousel
+                data={carouselItems}
+                renderItem={}
+                sliderWidth={SLIDER_WIDTH}
+                itemWidth={ITEM_WIDTH}
+                useScrollView={true}
+             />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+})
